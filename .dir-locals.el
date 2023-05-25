b788518aa7e451fcff3709f7c447804eabc91c14
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-05-25 13:45:20 +0200

    Add local environment files

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((compile-command . (format "make -f %s push" (get-closest-pathname "Makefile.emacs")))
+         (compilation-read-command . nil))))
