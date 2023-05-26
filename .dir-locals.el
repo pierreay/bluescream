755848520447cab85b2f50876e9838ac0bf20d76
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-05-26 22:13:56 +0200

    Delete old way to synchronize repo across machines

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil . ((compile-command . (format "make -f %s push" (get-closest-pathname "Makefile.emacs")))
-         (compilation-read-command . nil))))
