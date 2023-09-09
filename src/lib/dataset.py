a998882a55a74f35ffc2975aea485a1581856ab3
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-09-09 15:25:00 +0200

    simplify attack and train set into one class

--- src/lib/dataset.py
+++ src/lib/dataset.py
@@ -8,90 +8,73 @@ import lib.input_generators as input_generators
 import lib.load as load
 import lib.log as l
 
-DatasetType = Enum('DatasetType', ['TRAINING', 'ATTACK'])
+SubsetType = Enum('SubsetType', ['TRAIN', 'ATTACK'])
 InputType = Enum('InputType', ['FIXED', 'VARIABLE'])
 InputGeneration = Enum('InputGeneration', ['REAL_TIME', 'INIT_TIME'])
 
 class Dataset():
     """Top-level class representing a dataset."""
-    file         = "dataset.pyc"
+    file = "dataset.pyc"
 
-    name         = None
-
-    training_set = None
-    attack_set   = None
+    train_set = None
+    attack_set = None
 
     def __init__(self, name):
         self.name = name
 
     def __str__(self):
         string = "name={}".format(self.name)
-        if self.training_set is not None:
-            string = "{}\ntraining_set:\n{}".format(string, str(self.training_set))
+        if self.train_set is not None:
+            string = "{}\ntrain_set:\n{}".format(string, str(self.train_set))
         if self.attack_set is not None:
             string = "{}\nattack_set:\n{}".format(string, str(self.attack_set))
         return string
 
     @staticmethod
+    def get_path(dir):
+        return path.join(dir, Dataset.file)
+
+    @staticmethod
     def is_pickable(dir):
-        return path.exists(path.join(dir, Dataset.file))
+        return path.exists(Dataset.get_path(dir))
 
     @staticmethod
     def pickle_load(dir):
-        if not path.exists(path.join(dir, Dataset.file)):
+        if not path.exists(Dataset.get_path(dir)):
             return None
-        with open(path.join(dir, Dataset.file), "rb") as f:
+        with open(Dataset.get_path(dir), "rb") as f:
             return pickle.load(f)
 
     def pickle_dump(self, dir):
-        with open(path.join(dir, Dataset.file), "wb") as f:
+        with open(Dataset.get_path(dir), "wb") as f:
             pickle.dump(self, f)
 
-    def add_set(self, set, type):
-        assert(type in DatasetType)
-        if type == DatasetType.TRAINING:
-            self.training_set = set
-        elif type == DatasetType.ATTACK:
-            self.attack_set = set
+    def add_set(self, subset):
+        assert(subset.subtype in SubsetType)
+        if subset.subtype == SubsetType.TRAIN:
+            self.train_set = subset
+        elif subset.subtype == SubsetType.ATTACK:
+            self.attack_set = subset
     
-class TrainingSet():
-    """Training dataset."""
-    dir = "train"
-    pt_type = InputType.VARIABLE
-    ks_type = InputType.VARIABLE
-
-    name = None
-    pt_gen = None
-    ks_gen = None
-
+class Subset():
+    """Train or attack subset."""
     nb_trace = 0
 
-    def __init__(self, name, pt_gen, ks_gen):
+    def __init__(self, name, subtype, pt_gen, ks_gen):
+        assert(subtype in SubsetType) 
         assert(pt_gen in InputGeneration and ks_gen in InputGeneration)
         self.name = name
+        self.subtype = subtype
         self.pt_gen = pt_gen
         self.ks_gen = ks_gen
-
-    def __str__(self):
-        return "name={}".format(self.name)
-
-class AttackSet():
-    """Attack dataset."""
-    dir = "attack"
-    pt_type = InputType.FIXED
-    ks_type = InputType.VARIABLE
-
-    name = None
-    pt_gen = None
-    ks_gen = None
-
-    nb_trace = 0
-
-    def __init__(self, name, pt_gen, ks_gen):
-        assert(pt_gen in InputGeneration and ks_gen in InputGeneration)
-        self.name = name
-        self.pt_gen = pt_gen
-        self.ks_gen = ks_gen
+        if self.subtype == SubsetType.TRAIN:
+            self.dir = "train"
+            self.pt_type = InputType.VARIABLE
+            self.ks_type = InputType.VARIABLE
+        elif self.subtype == SubsetType.ATTACK:
+            self.dir = "attack"
+            self.pt_type = InputType.FIXED
+            self.ks_type = InputType.VARIABLE
 
     def __str__(self):
         return "name={}".format(self.name)
