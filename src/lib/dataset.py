c368c591676bcda398ee6622180237284f9f0f1e
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-09-09 14:06:28 +0200

    first version of pickable dataset

--- src/lib/dataset.py
+++ src/lib/dataset.py
@@ -1,12 +1,16 @@
 """Classes representing dataset."""
 
+import path
+import pickle
+
 DatasetType = Enum('DatasetType', ['TRAINING', 'ATTACK'])
 InputType = Enum('InputType', ['FIXED', 'VARIABLE'])
 InputGeneration = Enum('InputGeneration', ['REAL_TIME', 'INIT_TIME'])
 
 class Dataset():
     """Top-level class representing a dataset."""
-
+    file         = "dataset.pyc"
+    
     name         = None
     training_set = None
     attack_set   = None
@@ -22,23 +26,36 @@ class Dataset():
             string = "{}\nattack_set:\n{}".format(string, str(self.attack_set))
         return string
 
+    @staticmethod
+    def is_pickable(path):
+        return path.exists(path.join(path, Dataset.file))
+
+    @staticmethod
+    def pickle_load(path):
+        with open(path.join(path, Dataset.file), "rb") as f:
+            return pickle.load(f)
+
+    def pickle_dump(self, path):
+        with open(path.join(path, Dataset.file), "wb") as f:
+            pickle.dump(self, f)
+
     def add_set(self, set, type):
         assert(type in DatasetType)
         if type == DatasetType.TRAINING:
             self.training_set = set
         elif type == DatasetType.ATTACK:
             self.training_set = set
-
+    
 class TrainingSet():
     """Training dataset."""
+    dir = "train"
+    pt_type = InputType.VARIABLE
+    ks_type = InputType.VARIABLE
 
     name = None
     nb_trace = 0
     pt_gen = None
     ks_gen = None
-    
-    pt_type = InputType.VARIABLE
-    ks_type = InputType.VARIABLE
 
     def __init__(self, name, nb_trace, pt_gen, ks_gen):
         assert(pt_gen in InputGeneration and ks_gen in InputGeneration)
@@ -52,14 +69,14 @@ class TrainingSet():
 
 class AttackSet():
     """Attack dataset."""
+    dir = "attack"
+    pt_type = InputType.FIXED
+    ks_type = InputType.VARIABLE
 
     name = None
     nb_trace = 0
     pt_gen = None
     ks_gen = None
-    
-    pt_type = InputType.FIXED
-    ks_type = InputType.VARIABLE
 
     def __init__(self, name, nb_trace, pt_gen, ks_gen):
         assert(pt_gen in InputGeneration and ks_gen in InputGeneration)
