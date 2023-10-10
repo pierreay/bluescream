cd9be7ca0db406c3371e0b204a1d92b66566c1bb
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-10-10 16:20:36 +0200

    [misc.sh] allows to automatically find hci address

--- src/lib/misc.sh
+++ src/lib/misc.sh
@@ -15,6 +15,11 @@ function find_hci() {
     hciconfig | head -n 1 | cut -f 1 -d ":"
 }
 
+# Echo the ADDRESS of the first HCI device found.
+function find_hci_addr() {
+    hciconfig | sed '2q;d' | awk '{print $(3)}'
+}
+
 function find_nrf_com() {
      nrfjprog --com | cut - -d " " -f 5
 }
