7bf4136cc5da5f509927e942d8ab14fad6ac041e
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-10-23 14:32:41 +0200

    [compress_raw] use 2 archives

--- src/utils/compress_raw.sh
+++ src/utils/compress_raw.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# $1 is the set name [e.g. raw].
+# $2 is the subset name [train | attack].
+function compress_subset() {
+    tar cvf dataset_$1_$2_$(date "+%Y-%m-%d_%H-%M-%S").tar.bz2  -I pbzip2 $1/$2
+}
+
 # Compress the RAW dataset in order to upload it.
-cd $ENVRC_DATASET_PATH && tar cvf $HOME/dataset_raw_$(date "+%Y-%m-%d_%H-%M-%S").tar.bz2 -I pbzip2 raw/train raw/attack
+cd $ENVRC_DATASET_PATH && compress_subset raw train
+cd $ENVRC_DATASET_PATH && compress_subset raw attack
