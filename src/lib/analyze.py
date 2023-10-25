cf7d09aa84b21a3dad018b9c73a26f4c3ef690dd
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-10-25 14:43:47 +0200

    [analyze] add complex conversion function

--- src/lib/analyze.py
+++ src/lib/analyze.py
@@ -291,6 +291,52 @@ def align_all(s, sr, template=None, tqdm_log=True):
     """
     return align_nb(s, len(s), sr, template if template is not None else s[0], tqdm_log)
 
+def complex_p2r(radii, angles):
+    """Complex polar to regular.
+
+    Convert a complex number from Polar coordinate to Regular (Cartesian)
+    coordinates.
+
+    The input and output is symmetric to the complex_r2p() function. RADII is
+    the magnitude while ANGLES is the angles in radians (default for
+    np.angle()).
+
+    Example using complex_r2p for a regular-polar-regular conversion:
+    > polar = complex_r2p(2d_ndarray_containing_iq)
+    > polar[0].shape
+    (262, 2629)
+    > polar[1].shape
+    (262, 2629)
+    > regular = complex_p2r(polar[0], polar[1])
+    > regular.shape
+    (262, 2629)
+    > np.array_equal(arr, regular)
+    False
+    > np.isclose(arr, regular)
+    array([[ True,  True,  True, ...,  True,  True,  True], ..., [ True,  True,  True, ...,  True,  True,  True]])
+
+    Source: https://stackoverflow.com/questions/16444719/python-numpy-complex-numbers-is-there-a-function-for-polar-to-rectangular-co?rq=4
+    """
+    return radii * np.exp(1j * angles)
+
+def complex_r2p(x):
+    """Complex regular to polar.
+
+    Convert a complex number from Regular (Cartesian) coordinates to Polar
+    coordinates.
+
+    The input X can be a 1) single complex number 2) a 1D ndarray of complex
+    numbers 3) a 2D ndarray of complex numbers. The returned output is a tuple
+    composed of a 1) two scalars (float32) representing magnitude and phase 2)
+    two ndarray containing the scalars.
+
+    Example using a 2D ndarray as input:
+    complex_r2p(arr)[0][1][0] -> magnitude of 1st IQ of 2nd trace.2
+    complex_r2p(arr)[1][0][1] -> phase of 2nd IQ of 1st trace.
+
+    Source: https://stackoverflow.com/questions/16444719/python-numpy-complex-numbers-is-there-a-function-for-polar-to-rectangular-co?rq=4
+    """
+    return np.abs(x), np.angle(x)
 def average(arr):
     """Return the average signal of all signals composing the ARR 2D numpy
     array."""
