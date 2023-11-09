c553d559df67c389d7569a9f7a6fe45d1bf6033b
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-11-09 12:32:58 +0100

    [dataset_copy] fix and increase paralllelization speed

--- src/utils/dataset_copy.sh
+++ src/utils/dataset_copy.sh
@@ -24,19 +24,17 @@ function subset_copy() {
         copy $1/$3/p.npy $2/$3/p.npy
         for (( i = 0; i < $4; i++ ))
         do
-            # NOTE: Hacky parallelization using launching 2 background copy
-            # every 0.2 seconds.
+            # NOTE: Hacky parallelization launching 2 background copy.
             if [[ -f $1/$3/${i}_trace_nf.npy ]]; then
                 copy $1/$3/${i}_trace_nf.npy $2/$3/${i}_trace_nf.npy &
             fi
             if [[ -f $1/$3/${i}_trace_ff.npy ]]; then
                 copy $1/$3/${i}_trace_ff.npy $2/$3/${i}_trace_ff.npy &
             fi
-            sleep 0.1
-            # NOTE: No more than 10 rsync in parallel.
-            while [[ $(pgrep rsync | wc -l) > 10 ]]; do
+            # NOTE: No more than 50 rsync in parallel.
+            while test $(pgrep rsync | wc -l) -ge 50; do
                 log_warn "Sleep waiting rsync background processes to terminate..."
-                sleep 10
+                sleep 1
             done
         done
     fi
