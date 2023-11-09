27a61472ebe79bac069860285e4e4a198d1fad07
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-11-09 14:04:03 +0100

    [utils/dataset_copy.sh] allows to disable parallel copy

--- src/utils/dataset_copy.sh
+++ src/utils/dataset_copy.sh
@@ -7,9 +7,15 @@ source ./lib/log.sh
 # cp stub using rsync.
 # $1 is [SRC...].
 # $2 is [DST].
+# If env $PARALLEL != 0, use background copy.
 function copy() {
     log_info "Copy $1 to $2..."
-    rsync -avz --progress $1 $2
+    # NOTE: Hacky parallelization launching 2 background copy.
+    if [[ $PARALLEL != 0 ]]; then
+        rsync -avz --progress $1 $2 &
+    else
+        rsync -avz --progress $1 $2
+    fi
 }
 
 # Copy a subset.
@@ -24,18 +30,19 @@ function subset_copy() {
         copy $1/$3/p.npy $2/$3/p.npy
         for (( i = 0; i < $4; i++ ))
         do
-            # NOTE: Hacky parallelization launching 2 background copy.
             if [[ -f $1/$3/${i}_trace_nf.npy ]]; then
-                copy $1/$3/${i}_trace_nf.npy $2/$3/${i}_trace_nf.npy &
+                copy $1/$3/${i}_trace_nf.npy $2/$3/${i}_trace_nf.npy
             fi
             if [[ -f $1/$3/${i}_trace_ff.npy ]]; then
-                copy $1/$3/${i}_trace_ff.npy $2/$3/${i}_trace_ff.npy &
+                copy $1/$3/${i}_trace_ff.npy $2/$3/${i}_trace_ff.npy
             fi
             # NOTE: No more than 50 rsync in parallel.
-            while test $(pgrep rsync | wc -l) -ge 50; do
-                log_warn "Sleep waiting rsync background processes to terminate..."
-                sleep 1
-            done
+            if [[ $PARALLEL != 0 ]]; then
+                while test $(pgrep rsync | wc -l) -ge 50; do
+                    log_warn "Sleep waiting rsync background processes to terminate..."
+                    sleep 1
+                done
+            fi
         done
     fi
 }
@@ -44,8 +51,9 @@ function subset_copy() {
 
 # Help.
 if [[ $# -lt 3 ]]; then
-    echo "Usage: dataset_copy.sh SRC DST NB_TRACE"
+    echo "Usage: dataset_copy.sh SRC DST NB_TRACE [-p]"
     echo "This script will (partially or completely) copy a dataset from SRC to DST using NB_TRACE number of traces."
+    echo "If -p is passed, use parallel copy."
     exit 0
 fi
 
@@ -53,6 +61,7 @@ fi
 SRC=$1
 DST=$2
 NB_TRACE=$3
+PARALLEL=${4-0}
 
 # Copy.
 copy $SRC/dataset.pyc $DST/dataset.pyc
