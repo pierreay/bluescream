ea7a1b2d25963c9fc8cdeedcef8d7414d6f87cfa
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-11-21 17:09:22 +0100

    Don't return trigger during finding AES
    Because unused

--- src/lib/analyze.py
+++ src/lib/analyze.py
@@ -205,13 +205,13 @@ def find_aes_configured(s, sr, nb_aes=1, plot=False):
     # Second version of find_aes used for attack set:
     # starts, trigger = analyze.find_aes(arr, sr, 8.1e6, 8.5e6, nb_aes, 1e4, -0.5e-4, flip=False)
     # Third version of find_aes used to train set using 10 MHz bandwidth:
-    starts, trigger = analyze.find_aes(s, sr, 2.9e6, 3.3e6, nb_aes=nb_aes, lp=1e4, offset=-0.5e-4, flip=True, plot=plot)
+    starts = analyze.find_aes(s, sr, 2.9e6, 3.3e6, nb_aes=nb_aes, lp=1e4, offset=-0.5e-4, flip=True, plot=plot)
     check_nb = len(starts) < (1.1 * nb_aes) and len(starts) > (0.8 * nb_aes)
     if check_nb is True:
         l.LOGGER.debug("#{} detected AES".format(len(starts)))
     else:
         raise Exception("Aberrant number of detected AES: {}".format(len(starts)))
-    return starts, trigger
+    return starts
 
 def find_aes(s, sr, bpl, bph, nb_aes=1, lp=0, offset=0, flip=True, plot=False):
     """Find the indexes of AES computations inside a trace.
@@ -221,8 +221,7 @@ def find_aes(s, sr, bpl, bph, nb_aes=1, lp=0, offset=0, flip=True, plot=False):
     approximately NB_AES number of AES. BPL, BPH, LP are the bandpass and
     lowpass filters values used to create the trigger signal.
 
-    Return the list of start indexes and the Triggers object used for the
-    trigger signal.
+    Return the list of start indexes.
 
     """
     assert(isinstance(s, np.ndarray))
@@ -253,7 +252,7 @@ def find_aes(s, sr, bpl, bph, nb_aes=1, lp=0, offset=0, flip=True, plot=False):
     if np.shape(peaks[peaks <= 0]) != (0,):
         l.LOGGER.warning("discard detected aes turned negatives because of the offset")
         peaks = peaks[peaks >= 0]
-    return peaks, trigger_l
+    return peaks
 
 def choose_signal(arr, i = -1):
     """From the ARR 2D numpy array, propose every sub-signals (1D numpy array)
@@ -426,7 +425,7 @@ def average_aes(arr, sr, nb_aes, template, plot_enable=True):
     """
     # * Find AES.
     try:
-        starts, trigger = analyze.find_aes_configured(arr, sr, nb_aes=nb_aes, plot=plot_enable)
+        starts = analyze.find_aes_configured(arr, sr, nb_aes=nb_aes, plot=plot_enable)
     except Exception as e:
         l.LOGGER.error("Error during finding AES: {}".format(e), stack_info=True)
         return None, template
