d851bd6ad09d83278a0f8d339aaa631052c21d7f
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-11-30 15:12:11 +0100

    Rename "org-attach" to "org"

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((org-mode . ((org-attach-id-dir . "./org-attach"))))
+((org-mode . ((org-attach-id-dir . "./org"))))
