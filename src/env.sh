6a04b1a4ff4748ea8311e8855fb02606e7507e0e
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-12-05 15:48:15 +0100

    Add the env.sh file meant to be sourced

---
+++ src/env.sh
@@ -0,0 +1,6 @@
+# Source this file to load the project environment (variables and functions) in the current bash shell.
+direnv allow .
+source lib/discover.sh 
+source lib/log.sh 
+source lib/misc.sh
+source lib/pywrap.sh
