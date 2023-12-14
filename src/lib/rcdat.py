bc0ca542c26e1fa5bc9ada61d389ece9cfaffa40
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-12-14 11:11:26 +0100

    Start turning attenuator code into a class

--- src/lib/rcdat.py
+++ src/lib/rcdat.py
@@ -25,77 +25,101 @@ EOF
 import usb.core
 import usb.util
 
-# Find our device.
-dev = usb.core.find(idVendor=0x20ce, idProduct=0x0023)
-if dev is None:
-    raise ValueError('Device not found!')
+class RCDAT():
+    """Mini-Circuits RCDAT."""
 
-for configuration in dev:
-    for interface in configuration:
-        ifnum = interface.bInterfaceNumber
-    if not dev.is_kernel_driver_active(ifnum):
-        continue
-    try:
-        dev.detach_kernel_driver(ifnum)
-    except (usb.core.USBError, e):
-        pass
+    # USB-based device.
+    dev = None
 
-# Set the active configuration. With no args, we use the first config.
-dev.set_configuration()
-SerialN = ""
-ModelN = ""
-Fw = ""
+    # Identification numbers.
+    serial_number = ""
+    model_number = ""
+    firmware_name = ""
 
-# Get the serial number.
-dev.write(1, "*:SN?") # Send "*:SN?" to endpoint address 1.
-sn = dev.read(0x81, 64)
-# Convert the serial number.
-i = 1
-while (sn[i] < 255 and sn[i] > 0):
-    SerialN = SerialN + chr(sn[i])
-    i = i + 1
+    def __init__(self):
+        """Instantiate a RCDAT attenuator over the USB bus.
 
-# Get the model number.
-dev.write(1, "*:MN?")
-mn = dev.read(0x81, 64)
-# Convert the model number.
-i = 1
-while (mn[i] < 255 and mn[i] > 0):
-    ModelN = ModelN + chr(mn[i])
-    i = i + 1
+        On error, raise a ValueError exception.
 
-# Get the firmware version.
-dev.write(1,"*:FIRMWARE?")
-sn = dev.read(0x81, 64)
-# Convert the firmware version.
-i = 1
-while (sn[i] < 255 and sn[i] > 0):
-    Fw = Fw + chr(sn[i])
-    i = i + 1
+        """
+        # Find our device.
+        self.dev = usb.core.find(idVendor=0x20ce, idProduct=0x0023)
+        if self.dev is None:
+            raise ValueError('Device not found!')
 
-# Print information.
-print(ModelN)
-print(SerialN)
-print(Fw)
+        # Detach any active kernel driver to enable I/O.
+        for configuration in self.dev:
+            for interface in configuration:
+                ifnum = interface.bInterfaceNumber
+            if not self.dev.is_kernel_driver_active(ifnum):
+                continue
+            try:
+                self.dev.detach_kernel_driver(ifnum)
+            except (usb.core.USBError, e):
+                pass
 
-# Set attenuation of channel 1 to 11.25 dB.
-dev.write(1, "*:CHAN:1:SETATT:11.25;")
-# Get the command response.
-resp = dev.read(0x81, 64)
-# Convert the command response.
-i = 1
-AttResp = ""
-while (resp[i] < 255 and resp[i] > 0):
-    AttResp = AttResp + chr(resp[i])
-    i = i + 1
-print(AttResp)
+        # Enable the first found configuration.
+        self.dev.set_configuration()
+        # Get informations about device.
+        self.cmd_send("*:SN?")
+        self.serial_number = self.cmd_recv()
+        self.cmd_send("*:MN?")
+        self.model_number = self.cmd_recv()
+        self.cmd_send("*:FIRMWARE?")
+        self.firmware_name = self.cmd_recv()
 
-# Get all channels attenuation.
-dev.write(1,"*:ATT?")
-resp = dev.read(0x81,64)
-i = 1
-AttResp = ""
-while (resp[i] < 255 and resp[i] > 0):
-    AttResp = AttResp + chr(resp[i])
-    i = i + 1
-print(AttResp)
+    def cmd_send(self, cmd):
+        """Send the command CMD representing by a string to the device."""
+        assert self.dev is not None
+        assert type(cmd) == str
+        # Send the command CMD to endpoint address 1.
+        self.dev.write(1, cmd)
+
+    def cmd_recv(self):
+        """Return the response to the previous command as a string."""
+        assert self.dev is not None
+        # Read a buffer a 64 bytes.
+        resp = ""
+        buf = self.dev.read(0x81, 64)
+        # Convert the response to a string.
+        i = 1
+        while (buf[i] < 255 and buf[i] > 0):
+            resp = resp + chr(buf[i])
+            i = i + 1
+        return resp
+
+    def __str__(self):
+        str = "Model number: {}\n".format(self.model_number)
+        str += "Serial number: {}\n".format(self.serial_number)
+        str += "Firmware name: {}\n".format(self.firmware_name)
+        return str
+
+if __name__ == "__main__":
+    # Instantiate a RCDAT attenuator.
+    rcdat = RCDAT()
+    # Print its informations.
+    print(rcdat)
+    # Exit.
+    exit(0)
+    
+    # Set attenuation of channel 1 to 11.25 dB.
+    dev.write(1, "*:CHAN:1:SETATT:11.25;")
+    # Get the command response.
+    resp = dev.read(0x81, 64)
+    # Convert the command response.
+    i = 1
+    AttResp = ""
+    while (resp[i] < 255 and resp[i] > 0):
+        AttResp = AttResp + chr(resp[i])
+        i = i + 1
+    print(AttResp)
+
+    # Get all channels attenuation.
+    dev.write(1,"*:ATT?")
+    resp = dev.read(0x81,64)
+    i = 1
+    AttResp = ""
+    while (resp[i] < 255 and resp[i] > 0):
+        AttResp = AttResp + chr(resp[i])
+        i = i + 1
+    print(AttResp)
