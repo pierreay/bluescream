e3ef2d73e9775e56173b1205ebd84d4c1e42fba4
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-12-15 17:18:39 +0100

    Implement the get_att command in control.py

--- src/control.py
+++ src/control.py
@@ -15,7 +15,7 @@ def cli(log, loglevel):
 
 @cli.command()
 @click.argument("att", type=int)
-def set_attenuation(att):
+def set_att(att):
     """Set the RCDAT atenuation to ATT in dB."""
     l.LOGGER.info("Set the programmable attenuation to {} dB".format(att))
     rcdat_ctrl = rcdat.RCDAT()
@@ -23,6 +23,13 @@ def set_attenuation(att):
     if ret != 0:
         l.LOGGER.error("An error has occured during attenuation configuration!")
         exit(ret)
+
+@cli.command()
+def get_att():
+    """Get the RCDAT atenuation."""
+    rcdat_ctrl = rcdat.RCDAT()
+    att = rcdat_ctrl.attenuation_get()
+    l.LOGGER.info("Programmable attenuation is set to {} dB".format(att))
 
 if __name__ == "__main__":
     cli()
