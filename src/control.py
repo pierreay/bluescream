c2c25a4205b59b4a89ace3c3406121f5fde8c8f6
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-12-15 14:58:41 +0100

    Add the control.py to control the environement

---
+++ src/control.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import math
+
+import click
+
+import lib.log as l
+import lib.rcdat as rcdat
+
+@click.group(context_settings={'show_default': True})
+@click.option("--log/--no-log", default=True, help="Enable or disable logging.")
+@click.option("--loglevel", default="DEBUG", help="Set the logging level.")
+def cli(log, loglevel):
+    """Setup control utility."""
+    l.configure(log, loglevel)
+
+@cli.command()
+@click.argument("att", type=int)
+def set_attenuation(att):
+    """Set the RCDAT atenuation to ATT in dB."""
+    l.LOGGER.info("Set the attenuation to {} dB equivalent to {:.3f} meters of distance".format(att, 10 ** ((att - 20 * math.log10(2.4e9) + 147.55) / 20)))
+    rcdat_ctrl = rcdat.RCDAT()
+    ret = rcdat_ctrl.attenuation_set(att)
+    if ret != 0:
+        l.LOGGER.error("An error has occured during attenuation configuration!")
+        exit(ret)
+
+if __name__ == "__main__":
+    cli()
