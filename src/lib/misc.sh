a0b5b8ea7cb6f29826b9bdf35518991bec8db330
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-12-22 12:29:45 +0100

    Re-organize misc.sh

--- src/lib/misc.sh
+++ src/lib/misc.sh
@@ -6,12 +6,7 @@
 
 source ./lib/discover.sh
 
-# * Functions
-
-# Compile and flash the Nimble firmware from another git repository.
-function firmware_compile_flash() {
-    (cd ~/git/screaming_channels_nimble && make all)
-}
+# * Bash helper
 
 # Evaluate a step depending on the chosen mode (incremental or equal).
 # If $1 is set to "equal", return 1 (and echo 1) if $2 is equal $3.
@@ -25,6 +20,13 @@ function eval_cond() {
     return 0
 }
 
+# * Firmware
+
+# Compile and flash the Nimble firmware from another git repository.
+function firmware_compile_flash() {
+    (cd ~/git/screaming_channels_nimble && make all)
+}
+
 # Usage: firmware_set_mode [train | attack] >/dev/null 2>&1
 function firmware_set_mode() {
     script=/tmp/script.minicom
@@ -35,6 +37,8 @@ EOF
     minicom -D $(find_nrf_com) -S $script >/dev/null 2>&1 &
     sleep 3
 }
+
+# * Environment / Setup
 
 # Reset devices connected to YepKit YKush USB hub.$
 # $1 set to 1 perform the reset, otherwise, ignore and log warn [default = 1]
@@ -59,6 +63,8 @@ function ykush_reset() {
         log_warn "Skip ykush reset because it is not enabled!"
     fi
 }
+
+# * Dataset
 
 # Display a tree of a dataset.
 # $1 is the path of the dataset [default = $ENVRC_DATASET_RAW_PATH]
