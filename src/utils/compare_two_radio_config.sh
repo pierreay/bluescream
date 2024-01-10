e9940a143b4a3654ed87829626d4c07c92b9ea5c
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-01-10 12:33:28 +0100

    Don't use DEBUG log in compare_two_radio_config.sh

--- src/utils/compare_two_radio_config.sh
+++ src/utils/compare_two_radio_config.sh
@@ -9,14 +9,14 @@ ITER=0
 # $1 is the parameter to modify.
 function record_all() {
     log_info listen
-    ./radio.py --dir $ENVRC_RADIO_DIR listen $ENVRC_NF_FREQ $ENVRC_FF_FREQ $ENVRC_SAMP_RATE --nf-id $ENVRC_NF_ID --ff-id $ENVRC_FF_ID --duration=$ENVRC_DURATION --gain=$1 &
+    ./radio.py --loglevel INFO --dir $ENVRC_RADIO_DIR listen $ENVRC_NF_FREQ $ENVRC_FF_FREQ $ENVRC_SAMP_RATE --nf-id $ENVRC_NF_ID --ff-id $ENVRC_FF_ID --duration=$ENVRC_DURATION --gain=$1 &
     sleep 5
     log_info instrument
-    ./radio.py --dir $ENVRC_RADIO_DIR instrument $ENVRC_DATASET_RAW_PATH attack $ENVRC_ATTACKER_ADDR $ENVRC_VICTIM_ADDR $ENVRC_VICTIM_PORT $ENVRC_CONFIG_FILE --idx 0 --radio
+    ./radio.py --loglevel INFO --dir $ENVRC_RADIO_DIR instrument $ENVRC_DATASET_RAW_PATH attack $ENVRC_ATTACKER_ADDR $ENVRC_VICTIM_ADDR $ENVRC_VICTIM_PORT $ENVRC_CONFIG_FILE --idx 0 --radio
     log_info extract
     radio_extract INFO --no-plot --overwrite
     log_info to-numpy
-    ./radio.py --dir $ENVRC_RADIO_DIR to-numpy /tmp/test_harmonic_$ITER.npy
+    ./radio.py --loglevel INFO --dir $ENVRC_RADIO_DIR to-numpy /tmp/test_harmonic_$ITER.npy
     log_info quit
     radio_quit
     ITER=$(( ITER + 1))
