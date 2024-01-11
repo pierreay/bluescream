8a321808e16f48833410da3b85b271f7c48bfd0c
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-01-11 20:10:07 +0100

    Adapt attack_sweep to new attack

--- src/utils/attack_sweep.sh
+++ src/utils/attack_sweep.sh
@@ -18,8 +18,7 @@ if [[ $# -gt 0 ]]; then
 fi
 
 # Default variable and configuration summary.
-indir=$ENVRC_DATASET_AVG_PATH
-template_length=200
+indir=~/storage/dataset/240110_single-leak-pairing-1m-lna_raw
 log_info "indir=$indir"
 
 poi_num=2
@@ -28,13 +27,16 @@ variable=p_xor_k
 
 log_info "attack sweep using the attack set"
 # XXX: Attack trace sweep could be parallelized inside attack.py using same code template as dataset.py/average.
-start=1000
-for (( i = 1; i <= 2000; i++ ))
+start=72000
+stop=75000
+template_length=400
+step=10
+for (( i = $start; i <= $stop; i += $step ))
 do
-    end=$(( $start + $template_length ))
-    echo -n "start=$start;"
-    echo -n "end=$end;"
-    start=$(( $start + 1 ))
-    # Discard stderr because of tqdm progress bar.
-    ./attack.py --no-log --norm --dataset-path $indir --start-point $start --end-point $end --num-traces $NBTRACE attack  --attack-algo pcc --variable p_xor_k 2>/dev/null | grep CORRECT
+    end=$(( $i + $template_length ))
+    echo "=============="
+    echo -n "start=$i;"
+    echo "end=$end"
+    # NOTE: Discard stderr because of tqdm progress bar.
+    ./attack.py --no-log --no-plot --norm --dataset-path $indir --start-point $i --end-point $end --num-traces 300 attack --attack-algo pcc --profile ~/git/screaming_channels_ble/data/profiles/231222_single-cable-balanced_raw/ --variable p_xor_k --align 2>/dev/null | grep "CORRECT\|PGE\|rounded"
 done
