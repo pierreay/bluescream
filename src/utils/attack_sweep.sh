71e3a8f1d64c71b0e4223b9a286113f33537b984
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-01-12 12:31:34 +0100

    Refactor attack_sweep.sh utility while using it on Reaper

--- src/utils/attack_sweep.sh
+++ src/utils/attack_sweep.sh
@@ -1,36 +1,23 @@
 #!/bin/bash -e
 
+# XXX: Attack trace sweep could be parallelized inside attack.py using same
+# code template as dataset.py/average() function, if really needed.
+
 source ./lib/log.sh
-source ./lib/pywrap.sh
 
-function help() {
-    cat << EOF
-Usage: attack_sweep.sh
-
-Run a profiled correlation attack sweeping over the attack trace to find the correct alignment.
-EOF
-}
-
-# Check for number of args.
-if [[ $# -gt 0 ]]; then
-    help
-    exit 1
-fi
-
-# Default variable and configuration summary.
-indir=~/storage/dataset/240110_single-leak-pairing-1m-lna_raw
-log_info "indir=$indir"
-
+dataset_dir=~/storage/dataset/240110_single-leak-pairing-1m-lna_raw
+profile_dir=~/git/screaming_channels_ble/data/profiles/231222_single-cable-balanced_raw/ 
 poi_num=2
-poi_spacing=2
+poi_spacing=1
 variable=p_xor_k
 
-log_info "attack sweep using the attack set"
-# XXX: Attack trace sweep could be parallelized inside attack.py using same code template as dataset.py/average.
-start=72000
-stop=75000
+# * Sweep over points
+
+start=73835
+stop=73845
 template_length=400
-step=10
+step=1
+num_traces=1000
 for (( i = $start; i <= $stop; i += $step ))
 do
     end=$(( $i + $template_length ))
@@ -38,5 +25,28 @@ do
     echo -n "start=$i;"
     echo "end=$end"
     # NOTE: Discard stderr because of tqdm progress bar.
-    ./attack.py --no-log --no-plot --norm --dataset-path $indir --start-point $i --end-point $end --num-traces 300 attack --attack-algo pcc --profile ~/git/screaming_channels_ble/data/profiles/231222_single-cable-balanced_raw/ --variable p_xor_k --align 2>/dev/null | grep "CORRECT\|PGE\|rounded"
+    ./attack.py --no-log --no-plot --norm --dataset-path $dataset_dir --start-point $i --end-point $end --num-traces $num_traces attack --attack-algo pcc --profile $profile_dir --num-pois $poi_num --poi-spacing $poi_spacing --variable $variable --align 2>/dev/null | grep "CORRECT\|PGE\|rounded"
 done
+
+# * Sweep over number of traces
+
+# Once the previous part has found the best points, use it and start sweeping
+# the number of traces.
+
+start=73837
+end=$(( $start + $template_length ))
+for (( i = 100; i <= 16000; i += $((i / 10)) ))
+do
+    echo "=============="
+    echo "num-traces=$i"
+    # NOTE: Discard stderr because of tqdm progress bar.
+    ./attack.py --no-log --no-plot --norm --dataset-path $dataset_dir --start-point $start --end-point $end --num-traces $i attack --attack-algo pcc --profile $profile_dir --num-pois $poi_num --poi-spacing $poi_spacing --variable $variable --align 2>/dev/null | grep "CORRECT\|PGE\|rounded"
+done
+
+# * Final attack
+
+poi_num=1
+poi_spacing=5
+start=73837
+num_traces=5835
+./attack.py --no-log --no-plot --norm --dataset-path $dataset_dir --start-point $start --end-point $end --num-traces $num_traces attack --attack-algo pcc --profile $profile_dir --num-pois $poi_num --poi-spacing $poi_spacing --variable $variable --align 2>/dev/null | grep "CORRECT\|PGE\|rounded"
