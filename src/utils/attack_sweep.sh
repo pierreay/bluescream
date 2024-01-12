5177bffac7650f151f040cc78d232d9012072190
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-01-12 13:20:40 +0100

    New refactor of attack_sweep

--- src/utils/attack_sweep.sh
+++ src/utils/attack_sweep.sh
@@ -13,20 +13,24 @@ variable=p_xor_k
 
 # * Sweep over points
 
-start=73835
+start=73830
 stop=73845
 template_length=400
-step=1
-num_traces=1000
-for (( i = $start; i <= $stop; i += $step ))
-do
-    end=$(( $i + $template_length ))
-    log_info "=============="
-    log_info "start=$i"
-    log_info "end=$end"
-    # NOTE: Discard stderr because of tqdm progress bar.
-    ./attack.py --no-log --no-plot --norm --dataset-path $dataset_dir --start-point $i --end-point $end --num-traces $num_traces attack --attack-algo pcc --profile $profile_dir --num-pois $poi_num --poi-spacing $poi_spacing --variable $variable --align 2>/dev/null | grep "CORRECT\|PGE\|rounded"
-done
+step=2
+num_traces=1500
+function sweep_over_points() {
+    for (( i = $start; i <= $stop; i += $step ))
+    do
+        end=$(( $i + $template_length ))
+        log_info "=============="
+        log_info "start=$i"
+        log_info "end=$end"
+        # NOTE: Discard stderr because of tqdm progress bar.
+        ./attack.py --no-log --no-plot --norm --dataset-path $dataset_dir --start-point $i --end-point $end --num-traces $num_traces attack --attack-algo pcc --profile $profile_dir --num-pois $poi_num --poi-spacing $poi_spacing --variable $variable --align 2>/dev/null | grep "CORRECT\|PGE\|rounded"
+    done
+}
+
+sweep_over_points
 
 # * Sweep over number of traces
 
@@ -35,18 +39,24 @@ done
 
 start=73837
 end=$(( $start + $template_length ))
-for (( i = 100; i <= 16000; i += $((i / 10)) ))
-do
-    log_info "=============="
-    log_info "num-traces=$i"
-    # NOTE: Discard stderr because of tqdm progress bar.
-    ./attack.py --no-log --no-plot --norm --dataset-path $dataset_dir --start-point $start --end-point $end --num-traces $i attack --attack-algo pcc --profile $profile_dir --num-pois $poi_num --poi-spacing $poi_spacing --variable $variable --align 2>/dev/null | grep "CORRECT\|PGE\|rounded"
-done
+function sweep_over_number_of_traces() {
+    for (( i = 100; i <= 16000; i += $((i / 10)) ))
+    do
+        log_info "=============="
+        log_info "num-traces=$i"
+        # NOTE: Discard stderr because of tqdm progress bar.
+        ./attack.py --no-log --no-plot --norm --dataset-path $dataset_dir --start-point $start --end-point $end --num-traces $i attack --attack-algo pcc --profile $profile_dir --num-pois $poi_num --poi-spacing $poi_spacing --variable $variable --align 2>/dev/null | grep "CORRECT\|PGE\|rounded"
+    done
+}
+
+sweep_over_number_of_traces
 
 # * Final attack
 
-poi_num=1
-poi_spacing=5
-start=73837
-num_traces=5835
-./attack.py --no-log --no-plot --norm --dataset-path $dataset_dir --start-point $start --end-point $end --num-traces $num_traces attack --attack-algo pcc --profile $profile_dir --num-pois $poi_num --poi-spacing $poi_spacing --variable $variable --align 2>/dev/null | grep "CORRECT\|PGE\|rounded"
+function final_attack() {
+    start=73837
+    num_traces=3987
+    ./attack.py --no-log --no-plot --norm --dataset-path $dataset_dir --start-point $start --end-point $end --num-traces $num_traces attack --attack-algo pcc --profile $profile_dir --num-pois $poi_num --poi-spacing $poi_spacing --variable $variable --align 2>/dev/null | grep "CORRECT\|PGE\|rounded"
+}
+
+final_attack
