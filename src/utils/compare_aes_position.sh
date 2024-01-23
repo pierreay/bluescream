50cac009221ec936ee6a42c5f0405796414d7466
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-01-23 14:02:24 +0100

    Use dedicated script configuration file

--- src/utils/compare_aes_position.sh
+++ src/utils/compare_aes_position.sh
@@ -20,23 +20,27 @@ export SR=24e6
 
 echo "$(date)" > output.log
 
+# Don't modify project's configuration file.
+SCRIPT_CONFIG_FILE="/tmp/$(basename ENVRC_CONFIG_FILE)"
+cp "$ENVRC_CONFIG_FILE" $SCRIPT_CONFIG_FILE
+
 # ** Instrumentation functions
 
 function config() {
     param_name="$1"
     param_value="$2"
     echo "$1=$2" | tee -a output.log
-    sed -i "s/$1 = .*/$1 = $2/g" "$ENVRC_CONFIG_FILE"
+    sed -i "s/$1 = .*/$1 = $2/g" "$SCRIPT_CONFIG_FILE"
 }
 
 function instrument() {
-    ./radio.py --dir "$ENVRC_RADIO_DIR" --loglevel DEBUG listen "$ENVRC_NF_FREQ" "$ENVRC_FF_FREQ" "$SR" --nf-id $ENVRC_NF_ID --ff-id $ENVRC_FF_ID --duration=4 --gain=76 & # >/dev/null 2>&1 &
+    ./radio.py --dir "$ENVRC_RADIO_DIR" --loglevel DEBUG --config "$SCRIPT_CONFIG_FILE" listen "$ENVRC_NF_FREQ" "$ENVRC_FF_FREQ" "$SR" --nf-id $ENVRC_NF_ID --ff-id $ENVRC_FF_ID --duration=4 --gain=76 & # >/dev/null 2>&1 &
     sleep 15
-    ./radio.py --dir "$ENVRC_RADIO_DIR" instrument "$ENVRC_DATASET_RAW_PATH" train "$ENVRC_ATTACKER_ADDR" "$ENVRC_VICTIM_ADDR" "$ENVRC_VICTIM_PORT" --idx 0 --config example # >/dev/null 2>&1
+    ./radio.py --dir "$ENVRC_RADIO_DIR" --config "$SCRIPT_CONFIG_FILE" instrument "$ENVRC_DATASET_RAW_PATH" train "$ENVRC_ATTACKER_ADDR" "$ENVRC_VICTIM_ADDR" "$ENVRC_VICTIM_PORT" --idx 0 --config example # >/dev/null 2>&1
     if [[ $? != 0 ]]; then
         echo "INSTRUMENTATION ERROR" > /tmp/radio-extract.log
     else
-        ./radio.py --dir "$ENVRC_RADIO_DIR" extract "$SR" 0 --no-plot --no-overwrite --no-exit-on-error --config 1_aes_weak 2>&1 | tee /tmp/radio-extract.log
+        ./radio.py --dir "$ENVRC_RADIO_DIR" --config "$SCRIPT_CONFIG_FILE" extract "$SR" 0 --no-plot --no-overwrite --no-exit-on-error --config 1_aes_weak 2>&1 | tee /tmp/radio-extract.log
         cat /tmp/radio-extract.log | grep -E "Position|ERROR" >> output.log
     fi
     ./radio.py quit # >/dev/null 2>&1
